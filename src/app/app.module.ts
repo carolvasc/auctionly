6971dbb81db542559d37adf082c6b0e49e961c23
Carol Vasconcelos <cvsouza04@gmail.com>
2020-06-05 23:42:29 -0300

    Alterações no componente principal (App)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 import { AppComponent } from './app.component';
+import { AuctionComponent } from './modules/auction/containers/auction.component';
+import { AuctionListingComponent } from './modules/auction/components/auction-listing/auction-listing.component';
+import { PoModule } from '@po-ui/ng-components';
+import { RouterModule } from '@angular/router';
+
+import ptBr from '@angular/common/locales/pt';
+registerLocaleData(ptBr)
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuctionComponent,
+    AuctionListingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PoModule,
+    RouterModule.forRoot([])
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "pt"
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
