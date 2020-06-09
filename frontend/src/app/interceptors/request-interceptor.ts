5863034eda49c936440ca767c84f2ea6a8c1952f
Carol Vasconcelos <cvsouza04@gmail.com>
2020-06-09 01:04:25 -0300

    Limpeza de codigo no interceptor

--- frontend/src/app/interceptors/request-interceptor.ts
+++ frontend/src/app/interceptors/request-interceptor.ts
@@ -1,45 +1,40 @@
 import { LoginService } from '../components/login/login.service'
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
-import { LoadingRequestService } from '../shared/loading-request.service';
 import { PoNotificationService } from '@po-ui/ng-components';
 
 @Injectable()
-export class RequestInterceptor implements HttpInterceptor{
+export class RequestInterceptor implements HttpInterceptor {
 
-  constructor(public loginService: LoginService, public loadingRequestService: LoadingRequestService, private poNotification: PoNotificationService,){}
+  constructor(
+    private loginService: LoginService,
+    private poNotification: PoNotificationService,
+  ) { }
 
   /**
-   * Funções deste intercept
-   * - Incluir no header de cada requisição o Token JWT
-   * - Controlar o serviço de loading
+   * Inclui o Token JWT em todas as requisições.
    */
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    //const token = this.loginService.getJwtToken();
-    const token = ''
+    const token = this.loginService.getTokenJwt();
     let authReq = req;
-    this.loadingRequestService.onRequestStarted();
 
-    if(token !== null){
-      authReq = req.clone({headers: req.headers.set('Authorization', `Bearer ${token}`)});
+    if (token !== null) {
+      authReq = req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) });
     }
-    
+
     return next.handle(authReq).pipe(
       map((event: HttpEvent<any>) => {
-        if (event instanceof HttpResponse) {
-          this.loadingRequestService.onRequestFinished();
-        }
         return event;
       }),
       catchError((e: HttpErrorResponse) => {
         this.poNotification.error(e.error.error);
+
         if (e.error.error === 'Token inválido.') {
-          // Função que faz o logout
+          this.loginService.logout();
         }
-        this.loadingRequestService.onRequestFinished();
         return throwError(e);
       })
     );
