c392d46e7c44d23622814d446dd7a0f2633b1ab7
Carol Vasconcelos <cvsouza04@gmail.com>
2020-06-08 23:57:35 -0300

    Limpeza de codigo no Login Service

--- frontend/src/app/components/login/login.service.ts
+++ frontend/src/app/components/login/login.service.ts
@@ -1,153 +1,60 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { User } from '../../classes/User';
-import { Subject, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { APP_CONFIG, IAppConfig } from '../../app.config';
 import { Router } from '@angular/router';
-import { PoNotificationService } from '@po-ui/ng-components';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-    private poNotification: PoNotificationService,
-    @Inject(APP_CONFIG) private config: IAppConfig
-  ) { }
+  constructor(private router: Router) { }
 
   user: User;
+  token: string = null;
   authenticated: boolean = true;
-  userSubject = new Subject();
-  userAuthenticating: Subject<boolean> = new Subject<boolean>();
 
   /**
-   * Realiza a autenticação do usuário e o preenchimento das informações numa variável pública que fica disponível no serviço.
-   * Em caso de erro, lança a mensagem de retorno do banco para o usuário.
-   * 
-   * @param login string login do usuário
-   * @param password string senha do usuário
-   */
-  login(login: string, password: string) {
-
-    const user = new User();
-    user.login = login;
-    user.password = password;
-
-    //Indica que o usuário está em processo de autenticação
-    this.userAuthenticating.next(true);
-
-    this.authUser(user)
-      .toPromise()
-      .then((response: any) => {
-        localStorage.setItem('token', response.data.token);
-        this.user = response.data.user;
-        this.router.navigate(['home']);
-        this.authenticated = true;
-        this.userAuthenticating.next(false);
-      })
-      .catch((e) => {
-        this.authenticated = false;
-        this.userAuthenticating.next(false);
-
-        this.poNotification.error(e.error);
-      });
-  }
-
-  /**
-   * Chama a rota de autenticação do usuário passando login e senha
-   * 
-   * @param user User informações de autenticação do usuário (login e senha)
-   */
-  private authUser(user: User): Observable<any> {
-    return this.http.post(`${this.config.apiUrl}/auth/authenticate`, user);
-  }
-
-  /**
-   * Realiza o logout do usuário excluindo o token do localstorage e redirecionando para a tela de login
+   * Realiza o logout do usuário e limpa o sessionStorage.
    */
   logout() {
-    const token = this.getJwtToken();
-
-    if (token !== null) {
-      localStorage.removeItem('token');
+    if (this.token !== null) {
+      sessionStorage.removeItem('PO_USER_LOGIN');
     }
 
-    this.authenticated = false;
     this.user = null;
+    this.token = null;
+
     this.router.navigate(['/login']);
   }
 
-  userHasRole() {
-    return true;
+  /**
+   * Retorna o valor do token.
+   */
+  getTokenJwt() {
+    return this.token;
   }
 
   /**
-   * Verifica se o usuário está autenticado validando o token.
-   * Quando o token é validado com sucesso, ele é renovado.
+   * Seta o valor do token.
+   * @param token 
    */
-  isAuthenticated(): Promise<boolean> {
-    // let token = this.getJwtToken();
-
-    // if (token !== null) {
-    //   this.authenticated = true;
-    // } else {
-    //   this.authenticated = false;
-    // }
-
-    // return false;
-    let token = this.getJwtToken();
-
-    if (token !== null) {
-      const objToken: Object = { token };
-
-      return this.authUser(objToken).toPromise()
-        .then((response) => {
-          if (response.data.token) {
-            localStorage.removeItem('token');
-            localStorage.setItem('token', response.data.token);
-            token = this.getJwtToken();
-            this.user = response.data.user;
-
-            return this.authenticated = true;
-          } else {
-            return this.authenticated = false;
-          }
-        }).catch((e) => {
-          return new Promise(
-            (resolve, reject) => {
-              if (this.getJwtToken() !== null) {
-                localStorage.removeItem('token');
-              }
-
-              this.authenticated = false;
-              this.router.navigate(['/login']);
-
-              this.poNotification.error(e.error);
-
-              resolve();
-            });
-        });
-    } else {
-      return new Promise(
-        (resolve, reject) => {
-          if (this.getJwtToken() !== null) {
-            localStorage.removeItem('token');
-          }
-
-          this.authenticated = false;
-
-          resolve();
-        });
-    }
+  setTokenJwt(token: string) {
+    this.token = token;
   }
 
   /**
-   * Retorna o token
+   * Retorna o usuário.
    */
-  getJwtToken() {
-    return localStorage.getItem('token');
+  getUser() {
+    return this.user;
   }
+
+  /**
+   * Seta o valor do usuário.
+   * @param user 
+   */
+  setUser(user) {
+    this.user = user;
+  }
+
 }
