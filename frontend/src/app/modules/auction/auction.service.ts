ac15ed23b37d81f52a9bcda6e7cbe19813f744e6
Carol Vasconcelos <cvsouza04@gmail.com>
2020-06-08 23:57:21 -0300

    Atualizações nas requisições do front

--- frontend/src/app/modules/auction/auction.service.ts
+++ frontend/src/app/modules/auction/auction.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { APP_CONFIG, IAppConfig } from '../../app.config';
 import { Auction } from '../../classes/Auction';
+import { IApiResponse } from '../../interfaces/ApiResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -15,39 +16,39 @@ export class AuctionService {
    * Salva um novo leilão
    * @param auction Dados do leilão
    */
-  saveAuction(auction: Auction): Observable<any> {
-    return this.http.post<any>(`${this.config.apiUrl}/auction`, auction)
+  saveAuction(auction: Auction): Observable<IApiResponse> {
+    return this.http.post<IApiResponse>(`${this.config.apiUrl}/auction`, auction)
   }
 
   /**
    * Retorna um único leilão
    * @param idClient ID do leilão a ser retornado
    */
-  getAuction(idAuction: number): Observable<any> {
-    return this.http.get<any>(`${this.config.apiUrl}/auction/${idAuction}`);
+  getAuction(idAuction: number): Observable<IApiResponse> {
+    return this.http.get<IApiResponse>(`${this.config.apiUrl}/api/auction/${idAuction}`);
   }
 
   /**
    * Atualiza os dados do leilão
    * @param leilao Leilão com os dados atualizados
    */
-  updateAuction(auction: Auction): Observable<any> {
-    return this.http.put<any>(`${this.config.apiUrl}/auction/${auction.id}`, auction);
+  updateAuction(auction: Auction): Observable<IApiResponse> {
+    return this.http.put<IApiResponse>(`${this.config.apiUrl}/api/auction/${auction._id}`, auction);
   }
 
   /**
    * Exclui o leilão
    * @param idAuction ID do leilão a ser removido
    */
-  deleteAuction(idAuction: number): Observable<any> {
-    return this.http.delete<any>(`${this.config.apiUrl}/auction/${idAuction}`);
+  deleteAuction(idAuction: number): Observable<IApiResponse> {
+    return this.http.delete<IApiResponse>(`${this.config.apiUrl}/api/auction/${idAuction}`);
   }
 
   /**
    * Retorna todos os leilões
    */
-  getAllAuctions(): Observable<any> {
-    return this.http.get<any>(`${this.config.apiUrl}/auction`);
+  getAllAuctions(): Observable<IApiResponse> {
+    return this.http.get<IApiResponse>(`${this.config.apiUrl}/api/auction/all`);
   }
 
 }
