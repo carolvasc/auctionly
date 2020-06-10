f547c51ea7487308f703185fba7b1ce60ae9182f
Carol Vasconcelos <cvsouza04@gmail.com>
2020-06-09 21:55:03 -0300

    Atualização dos modulos e remoção dos componentes nao usados

--- frontend/src/app/modules/auction/auction.module.ts
+++ frontend/src/app/modules/auction/auction.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AuctionRoutingModule } from './auction-routing.module';
 import { AuctionService } from './auction.service';
+import { AuctionRoutingModule } from './auction-routing.module';
 import { AuctionRegisterComponent } from './components/auction-register/auction-register.component';
+// PO UI Components
 import { PoDynamicModule } from '@po-ui/ng-components';
 import { PoButtonModule } from '@po-ui/ng-components';
 import { AuctionRegisterResolver } from './guards/auction-register.resolver';
 import { PoFieldModule } from '@po-ui/ng-components';
 import { PoInfoModule } from '@po-ui/ng-components';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { PoWidgetModule } from '@po-ui/ng-components';
+// Interceptor
 import { RequestInterceptor } from '../../interceptors/request-interceptor';
+import { AuctionListingComponent } from './components/auction-listing/auction-listing.component';
 
 @NgModule({
   imports: [
@@ -23,9 +27,11 @@ import { RequestInterceptor } from '../../interceptors/request-interceptor';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    PoWidgetModule,
   ],
   declarations: [
-    AuctionRegisterComponent
+    AuctionRegisterComponent,
+    AuctionListingComponent,
   ],
   providers: [
     AuctionService,
