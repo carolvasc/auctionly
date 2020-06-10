02783b390842b72a4052ea9c52dea459dca0afca
Carol Vasconcelos <cvsouza04@gmail.com>
2020-06-09 21:54:40 -0300

    Atualização na Home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_CONFIG, appConfig } from './app.config';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import ptBr from '@angular/common/locales/pt';
-
-import { RequestInterceptor } from "./interceptors/request-interceptor";
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -19,10 +16,7 @@ import { PoPageLoginModule } from '@po-ui/ng-templates';
 
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { AuctionListingComponent } from './modules/auction/components/auction-listing/auction-listing.component';
 
-import {SharedModule} from './shared/shared.module';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuctionModule } from './modules/auction/auction.module';
 
 registerLocaleData(ptBr)
@@ -32,7 +26,6 @@ registerLocaleData(ptBr)
     AppComponent,
     HomeComponent,
     LoginComponent,
-    AuctionListingComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,7 +33,6 @@ registerLocaleData(ptBr)
     PoPageLoginModule,
     AppRoutingModule,
     AuctionModule,
-    SharedModule,
     RouterModule.forRoot([])
   ],
   providers: [
@@ -52,11 +44,6 @@ registerLocaleData(ptBr)
     {
       provide: LOCALE_ID,
       useValue: "pt"
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: RequestInterceptor,
-      multi: true,
     },
   ],
   bootstrap: [AppComponent]
